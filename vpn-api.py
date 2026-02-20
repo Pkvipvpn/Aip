adfbdb2525009394459f1ce13e602b6be579a366
Pkvipvpn <125269195+Pkvipvpn@users.noreply.github.com>
2026-02-20 08:51:48 +0700

    Update vpn-api.py

--- vpn-api.py
+++ vpn-api.py
@@ -3,37 +3,38 @@ import sqlite3
 from datetime import datetime
 
 app = FastAPI()
-DB="users.db"
+
+DB="/root/vpn_api/users.db"
 
 @app.get("/")
 def get_users():
+
     conn = sqlite3.connect(DB)
     c = conn.cursor()
     c.execute("SELECT name, hwid, expire, status FROM users")
     rows = c.fetchall()
     conn.close()
 
-    result = []
+    result=[]
 
-    for row in rows:
-        name, hwid, expire, status = row
+    for name,hwid,expire,status in rows:
 
-        if status != "active":
+        if status!="active":
             continue
 
         try:
-            expire_date = datetime.strptime(expire, "%Y-%m-%d")
-            days_left = (expire_date - datetime.now()).days
+            expire_date=datetime.strptime(expire,"%Y-%m-%d")
+            days_left=(expire_date-datetime.now()).days
         except:
-            days_left = 0
+            days_left=0
 
-        if days_left < 0:
-            days_left = 0
+        if days_left<0:
+            days_left=0
 
         result.append({
-            "Name": name,
-            "Key": hwid,
-            "Valid": str(days_left)
+            "Name":name,
+            "Key":hwid,
+            "Valid":str(days_left)
         })
 
     return result
